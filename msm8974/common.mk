3f0034b8ce994df3f48e5b96c4f9e87edbedbf54
Alice Weiss <admin@2x2ch.ru>
2016-09-07 11:58:03 +0300

    Add kernel headers
    Change-Id: Ic3b1caea8bc95b8474f5cfecee0a3f8271c873df

--- msm8974/common.mk
+++ msm8974/common.mk
@@ -29,8 +29,9 @@ ifeq ($(TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS), true)
     common_flags += -DFORCE_HWC_FOR_VIRTUAL_DISPLAYS
 endif
 
-common_deps  :=
-kernel_includes :=
+common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
 
 # Executed only on QCOM BSPs
 ifeq ($(TARGET_USES_QCOM_BSP),true)
